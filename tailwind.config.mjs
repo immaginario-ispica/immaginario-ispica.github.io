2f4b36080f5bd57c4a515618dba25fdac0afade1
Francesco Lauretta <6022056+FrancescoLauretta@users.noreply.github.com>
2025-04-18 10:05:17 +0100

    v0.1

---
+++ tailwind.config.mjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f8f8f8',
+          100: '#f0f0f0',
+          200: '#e4e4e4',
+          300: '#d1d1d1',
+          400: '#b4b4b4',
+          500: '#9a9a9a',
+          600: '#818181',
+          700: '#6a6a6a',
+          800: '#5a5a5a',
+          900: '#4e4e4e',
+          950: '#282828',
+        },
+        accent: {
+          50: '#f5f5f5',
+          100: '#e9e9e9',
+          200: '#d9d9d9',
+          300: '#c4c4c4',
+          400: '#9d9d9d',
+          500: '#7b7b7b',
+          600: '#555555',
+          700: '#434343',
+          800: '#262626',
+          900: '#171717',
+          950: '#0a0a0a',
+        }
+      },
+      fontFamily: {
+        serif: ['EB Garamond', 'Garamond', 'Georgia', 'serif'],
+        sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      backgroundImage: {
+        'pattern': "url('/pattern.png')"
+      }
+    },
+  },
+  plugins: [],
+};
